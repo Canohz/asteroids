8ad5fb6273b099ac01124bd92ea01e2b0c9db5a2
Canohz <iudexgold@gmail.com>
2025-06-23 14:32:30 -0400

    added player triangle

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
                 return
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.get_time() / 1000.0  # Convert milliseconds to seconds
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
     main()
